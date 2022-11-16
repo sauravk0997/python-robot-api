db29884ddc74905374b5131d305a36ef2a8a6060
yusuf Mubarak <scientist07861@gmail.com>
2022-11-16 19:39:02 +0530

    minor changes

--- lib/schemas/MovePlayerSchema.py
+++ lib/schemas/MovePlayerSchema.py
@@ -7,35 +7,35 @@ class ItemsSchema(Schema):
     class Meta:
         unknown = RAISE
 
-    fromLineupSlotId = fields.Int(required=True)
-    fromTeamId = fields.Int(required=True)
-    isKeeper = fields.Bool(required=True)
-    overallPickNumber = fields.Int(required=True)
-    playerId = fields.Int(required=True)
-    toLineupSlotId = fields.Int(required=True)
-    toTeamId = fields.Int(required=True)
-    type = fields.String(required=True)
+    fromLineupSlotId      = fields.Int(required=True)
+    fromTeamId            = fields.Int(required=True)
+    isKeeper              = fields.Bool(required=True)
+    overallPickNumber     = fields.Int(required=True)
+    playerId              = fields.Int(required=True)
+    toLineupSlotId        = fields.Int(required=True)
+    toTeamId              = fields.Int(required=True)
+    type                  = fields.String(required=True)
 
 class MovePlayerSchema(Schema):
     class Meta:
         unknown = RAISE
 
-    bidAmount = fields.Int(required=True)
-    executionType = fields.String(required=True)
-    id = fields.String(required=True)
-    isActingAsTeamOwner = fields.Bool(required=True)
-    isLeagueManager = fields.Bool(required=True)
-    isPending = fields.Bool(required=True)
-    items = fields.List(fields.Nested(ItemsSchema), required=True)
-    memberId = fields.String(required=True)
-    proposedDate = fields.Int(required=True)
-    rating = fields.Int(required=True)
-    scoringPeriodId = fields.Int(required=True)
+    bidAmount               = fields.Int(required=True)
+    executionType           = fields.String(required=True)
+    id                      = fields.String(required=True)
+    isActingAsTeamOwner     = fields.Bool(required=True)
+    isLeagueManager         = fields.Bool(required=True)
+    isPending               = fields.Bool(required=True)
+    items                   = fields.List(fields.Nested(ItemsSchema), required=True)
+    memberId                = fields.String(required=True)
+    proposedDate            = fields.Int(required=True)
+    rating                  = fields.Int(required=True)
+    scoringPeriodId         = fields.Int(required=True)
     skipTransactionCounters = fields.Bool(required=True)
-    status = fields.String(required=True)
-    subOrder = fields.Int(required=True)
-    teamId = fields.Int(required=True)
-    type = fields.String(required=True)
+    status                  = fields.String(required=True)
+    subOrder                = fields.Int(required=True)
+    teamId                  = fields.Int(required=True)
+    type                    = fields.String(required=True)
 
 
 if __name__ == '__main__':
--- testsuite/move-players-test.robot
+++ testsuite/move-players-test.robot
@@ -1,13 +1,13 @@
 *** Settings ***
-Documentation       Moving players form a fantasy league Positive tests are executing along with schema validation
-...                 to run: robot --pythonpath $PWD ./testsuite/Move-Players-test.robot
-Metadata    Author      Yusuf Mubarak M
-Metadata    Date        14-11-2022
-Library    RequestsLibrary
-Library    Collections
-Library    OperatingSystem
-Library     RPA.JSON
-Resource    resource/FantasyLeagueResource.Robot
+Documentation        Moving players form a fantasy league Positive tests are executing along with schema validation
+...                  to run: robot --pythonpath $PWD ./testsuite/Move-Players-test.robot
+Metadata             Author      Yusuf Mubarak M
+Metadata             Date        14-11-2022
+Library              RequestsLibrary
+Library              Collections
+Library              OperatingSystem
+Library              RPA.JSON
+Resource             resource/FantasyLeagueResource.Robot
 
 *** Test Cases ***
 Move the Players by swaping the position of the players
@@ -20,10 +20,6 @@ Move the Players by swaping the position of the players
     Validate players swapped their positions ${swapped_response}
 
 Move the Players to Bench
-    [Tags]    moveplayers_to_bench  valid  CSEAUTO-28395
+    [Tags]    moveplayers-to-bench  valid  CSEAUTO-28395
     ${move_to_bench_response}     Move any player to bench and validate the response schema
     Validate player is moved to bench ${move_to_bench_response}
-
-
-
-
